cd686cd7f317089415e65b3d44f34be69bb6014d
PKingTeak <rudxorrmrwkd@gmail.com>
2024-01-29 16:34:43 +0900

    이미지 로드 하는방법 엔진 Base안에서 Directory File path 생성

--- Project/WinAPI/Contents/User.cpp
+++ Project/WinAPI/Contents/User.cpp
@@ -22,9 +22,9 @@ void User::BeginPlay()
 
 
 	{
-		PlayerRenderer = CreateImageRenderer(0);
-		PlayerRenderer->SetPosition({ 200, 500 });
-		PlayerRenderer->SetScale({ 100,50 });
+	//	PlayerRenderer = CreateImageRenderer(0);
+	//	PlayerRenderer->SetPosition({ 200, 500 });
+	//	PlayerRenderer->SetScale({ 100,50 });
 		//이건 랜더러의 크기이고 	
 	}
 	SetActorLocation({ 200, 500 });
