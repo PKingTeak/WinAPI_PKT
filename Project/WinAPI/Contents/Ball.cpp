2cc4b3dd00a7e088e01a37ec8ccb7b728fbecd53
PKingTeak <rudxorrmrwkd@gmail.com>
2024-02-14 13:07:48 +0900

    [구현]  공 반사 구현 완료 반사각 활용으로

--- Project/WinAPI/Contents/Ball.cpp
+++ Project/WinAPI/Contents/Ball.cpp
@@ -17,10 +17,11 @@ void Ball::BeginPlay()
 	
 	AActor::BeginPlay();
 	
-	SetActorLocation({ User::CurPos.X,User::CurPos.Y - 10 });
+	
+	
 	BallRender = CreateImageRenderer(0);
 	UEngineResourcesManager::GetInst().CuttingImage("Ball.png", 1, 1);
-	Move({ User::CurPos.X,User::CurPos.Y - 10 });
+	
 	BallRender->SetImage("Ball.png"); 
 	BallRender->SetScale({ 10,8 });
 
@@ -28,6 +29,7 @@ void Ball::BeginPlay()
 }
 void Ball::SetPos(FVector _CurPos)
 {
+	
 	CurBallPos = _CurPos;
 
 }
@@ -35,16 +37,22 @@ void Ball::SetPos(FVector _CurPos)
 
 void Ball::Tick(float _DeltaTime)
 {
-	
+	if (false == IsballLive)
+	{
+	SetActorLocation({ User::CurPos.X,User::CurPos.Y - 10 });
+	}
 	Time += _DeltaTime;
 	AActor::Tick(_DeltaTime);
 	GameStart(Time);
 	DirCheck();
 	if (IsballLive == true)
 	{
-	AddActorLocation(BDir * Speed * _DeltaTime);//공이이동한다.
+		Move(_DeltaTime);
 	}
-	//어쩌피 방향벡터를 정해줬으니 구지 필요없는거 아닐까?
+	else if (UEngineInput::IsDown(' '))
+	{
+		IsballLive = true;
+	}
 	
 }
 
@@ -53,20 +61,24 @@ void Ball::DirCheck()
 	CurBallPos = GetActorLocation(); //내 위치를 알아야 하니까 
 	if (CurBallPos.X <= 5)
 	{
-		BDir = { 1,1/*진행방향으로 가고픔*/}; //여기를 변경하면 될듯 하다. 방향으로 하지말고 벡터값을 주면서 변경을 시켜보자. 
+		Reflect(CurBallPos);
+		//BDir = { 1,1/*진행방향으로 가고픔*/}; //여기를 변경하면 될듯 하다. 방향으로 하지말고 벡터값을 주면서 변경을 시켜보자. 
 	}
 	else if (CurBallPos.X >= 500)
 	{
-		BDir = {-1,-1};
+		//BDir = {-1,-1};
+		Reflect(CurBallPos);
 	}
 
 	if (CurBallPos.Y <= 50)
 	{
-		
-		BDir = {-1,1};
+		Reflect(CurBallPos);
+		//YReflect();
+		//BDir = {-1,1};
 	}
 	else if (CurBallPos.Y >= User::CurPos.Y-10)
 	{
+		Reflect(CurBallPos);
 		//원래 공을 제거 해야됨 시작 위치로 다시 위치시켜야됨
 	
 		BDir = {1,-1};
@@ -95,9 +107,76 @@ void Ball::GameStart(float _DeltaTime)
 }
 
 
-void Ball::Move(FVector _StartPos)
+void Ball::StartPos(FVector _StartPos)
 {
 	CurBallPos = _StartPos; //시작 위치 
 	
 }
 
+void Ball::Block()
+{
+	
+}
+
+
+void Ball::Move(float _DeltaTime)
+{
+	BDir.Normalize2D();
+
+	AddActorLocation(BDir * Speed * _DeltaTime);//공이이동한다.
+
+	//이걸로 각도 조절하면될듯 하다. 
+}
+void Ball::Reflect(FVector _CurBallPos)
+{
+	FVector CurBallPos = _CurBallPos;
+	if (CurBallPos.X >= 500)
+	{
+
+	FVector N = { -1,0 };
+	FVector N2 = { -2,0 };
+	FVector T = { (-1 * BDir.X) * N.X,(-1 * BDir.Y) * N.Y }; //(-P * n)
+	BDir = BDir + (N2 * T); //R
+	//N을 구하는걸 못하겠음?
+	}
+	else if(CurBallPos.X <= 0 )
+	{
+		FVector N = { 1,0 };
+		FVector N2 = { 2,0 };
+		FVector T = { (-1 * BDir.X) * N.X,(-1 * BDir.Y) * N.Y }; //(-P * n)
+		BDir = BDir + (N2 * T); //R
+	}
+	else if (CurBallPos.Y >= 0)
+	{
+		FVector N = { 0,1 };
+		FVector N2 = { 0,2 };
+		FVector T = { (-1 * BDir.X) * N.X,(-1 * BDir.Y) * N.Y }; //(-P * n)
+		BDir = BDir + (N2 * T); //R
+
+	}
+	else if (CurBallPos.Y <= User::CurPos.Y)
+	{
+		FVector N = { 0,-1 };
+		FVector N2 = { 0,-2 };
+		FVector T = { (-1 * BDir.X) * N.X,(-1 * BDir.Y) * N.Y }; //(-P * n)
+		BDir = BDir + (N2 * T); //R
+	}
+
+
+	
+	
+	//
+	int a = 0;
+}
+ 
+//void Ball::YLReflect()
+//{	  //
+//	  //
+//}	  //
+//
+/*
+
+R= P + 2n(-P * n)
+
+BDir =  BDir +NCurBallPos(Cos);
+*/
--- Project/WinAPI/Contents/Ball.h
+++ Project/WinAPI/Contents/Ball.h
@@ -18,7 +18,7 @@ public:
 	//공의 기능을 생각해 봤을때 
 	/*
 	1. 시작점 지정  -> 플레이어 위치에 지정 
-		이거는 플레이어 내부에 있는 CurPos를 사용해야 하기 때문에 User에서 구현하는게 좋을듯
+		변경 플레이어 위치를 가져와서 따라다니게 함 isballLive는 공이 맵에 존재하는지 채크하는것이다. 
 	2. 몇초후 시작 혹은 스페이스바를 누르면 바로 앞으로 진행
 		맵상에서 공은 무조건 한개만 존재 해야됨 == bool타입으로 공이 생존했는지 확인해보자
 	3. 공이 떨어져서 사라지면 다시 생기는 시작점 = 플레이어 위치
@@ -27,18 +27,22 @@ public:
 
 	*/
 	void SetPos(FVector _CurPos);
-	void Move(FVector _StartPos);
+	void StartPos(FVector _StartPos);
 	void DirCheck();
+	void Reflect(FVector _CurBallPos);
+	//void YLReflect();
 protected:
 	void BeginPlay() override;
 	void Tick(float _DeltaTime) override;
 	
 	
 private:
+	void Move(float _DeltaTime);
+	void Block();
 	void GameStart(float _DeltaTime);
 	UImageRenderer* BallRender = nullptr;
 	FVector CurBallPos = {};
-	FVector BDir = {0,0};
+	FVector BDir = {1,-1};
 	bool IsballLive = false;
 	float Speed = 300.0f;
 	float Time = 0.0f;
